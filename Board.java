e4153d320346519315700b847d8d4bb4102b4a53
blgyycw <92336269+blgyycw@users.noreply.github.com>
2024-02-20 12:51:06 -0800

    Create Board.java
    our board class
    
    still not sure if it should all be in the same file or reorganized
    still not sure about determining draws
    
    for now please just work on generateLegalMoves of each piece, keep in mind the new move should not put the current king in check.

---
+++ Board.java
@@ -0,0 +1,61 @@
+import java.util.ArrayList;
+public class Board {
+    private abstract class Piece {
+        private boolean pieceColor; //true means white
+        private int row;
+        private int column;
+        public void move(int r,int c) {
+            pieces[r][c]=this;
+            pieces[row][column]=null;
+            row=r;
+            column=c;
+        }
+        public abstract ArrayList<int[]> generateLegalMoves();
+    }
+    private class Pawn extends Piece {
+        public ArrayList<int[]> generateLegalMoves() {
+            return null;
+        }
+    }
+    private class Knight extends Piece {
+        public ArrayList<int[]> generateLegalMoves() {
+            return null;
+        }
+    }
+    private class Bishop extends Piece {
+        public ArrayList<int[]> generateLegalMoves() {
+            return null;
+        }
+    }
+    private class Queen extends Piece {
+        public ArrayList<int[]> generateLegalMoves() {
+            return null;
+        }
+    }
+    private class King extends Piece {
+        public ArrayList<int[]> generateLegalMoves() {
+            return null;
+        }
+    }
+    private Piece[][] pieces;
+    private boolean inCheck;
+    private boolean whitesTurn;
+    private boolean castleWhiteLeft;
+    private boolean castleWhiteRight;
+    private boolean castleBlackLeft;
+    private boolean castleBlackRight;
+    private int moves; //for tracking draws
+    private ArrayList<Integer> repeatedPositions;
+    /**Arrange pieces into the default starting position.**/
+    public Board() {
+    }
+    public Piece[][] getBoard() {return pieces;}
+    /* trash
+    public Board(Piece[][] pieces) {
+        this.pieces=pieces;
+    }
+    */
+    //idk yet
+    public void endGame() {}
+    
+}
