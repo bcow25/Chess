fc28f6e0da3eb77bc07639ec6bfaf3ca46ce95a6
Swing-Rock <85258557+Swing-Rock@users.noreply.github.com>
2024-02-21 12:09:35 -0800

    Update Board.java
    contructors

--- Board.java
+++ Board.java
@@ -1,38 +1,59 @@
 import java.util.ArrayList;
 public class Board {
     private abstract class Piece {
+        public Piece (int r, int c){
+            row = r; 
+            col = c;
+        }
         protected boolean pieceColor; //true means white
         protected int row;
-        protected int column;
+        protected int col;
         public void move(int r,int c) {
             pieces[r][c]=this;
-            pieces[row][column]=null;
+            pieces[row][col]=null;
             row=r;
-            column=c;
+            col=c;
         }
         public abstract ArrayList<int[]> generateLegalMoves();
     }
     private class Pawn extends Piece {
+        public Pawn(int r, int c){
+            super(r,c); 
+        }
+            
+        
         public ArrayList<int[]> generateLegalMoves() {
             return null;
         }
     }
     private class Knight extends Piece {
+        public Knight(int r, int c){
+            super(r,c); 
+        }
         public ArrayList<int[]> generateLegalMoves() {
             return null;
         }
     }
     private class Bishop extends Piece {
+        public Bishop(int r, int c){
+            super(r,c); 
+        }
         public ArrayList<int[]> generateLegalMoves() {
             return null;
         }
     }
     private class Queen extends Piece {
+        public Queen(int r, int c){
+            super(r,c); 
+        }
         public ArrayList<int[]> generateLegalMoves() {
             return null;
         }
     }
     private class King extends Piece {
+        public King(int r, int c){
+            super(r,c); 
+        }
         public ArrayList<int[]> generateLegalMoves() {
             return null;
         }
