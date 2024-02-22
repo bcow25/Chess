017022b66a1ccf51d4cedc4961bbf48720f078b1
Swing-Rock <85258557+Swing-Rock@users.noreply.github.com>
2024-02-22 12:45:46 -0800

    Add in Pawn
    hopes and prayers

--- Board.java
+++ Board.java
@@ -17,13 +17,81 @@ public class Board {
         public abstract ArrayList<int[]> generateLegalMoves();
     }
     private class Pawn extends Piece {
-        public Pawn(int r, int c){
-            super(r,c); 
+        private boolean firstMove;
+        private boolean canGetFrenched; //en passant 
+        //constructor 
+        public Pawn(int r, int c, boolean color){
+            super(r,c, color); 
+            firstMove = true; 
+            canGetFrenched = false; 
         }
-            
+        
+        public void setFirstMove(boolean bool){ firstMove = bool; }
+        public void setCanGetFrenched(boolean bool){ canGetFrenched = bool; }
+        public boolean getFirstMove(){ return firstMove;  }
+        public boolean getCanGetFrenched(){ return canGetFrenched; }
         
         public ArrayList<int[]> generateLegalMoves() {
-            return null;
+            ArrayList<int[]> moves = new ArrayList<int[]>(); 
+            if(firstMove){//if is Pawn's first move then can move two blocks 
+                if(pieceColor){ // if is white 
+                    if(pieces[row-2][col] == null) // if 2 space ahead is unoccupied, add to moves
+                        moves.add( new int[] {row - 2, col} );                     
+                } else { //color is black 
+                    if(pieces[row+2][col] == null) // if 2 space ahead is unoccupied, add to moves
+                        moves.add( new int[] {row + 2, col} ); 
+                }                                   
+            }
+             //check if pawn can move forward
+             if(pieceColor){// if is white
+                 if(row != 0 && pieces[row-1][col] == null) // if not on top row and space ahead is unoccupied, add to moves
+                    moves.add( new int[] {row - 1, col} ); 
+             } else { //color is black 
+                 if(row != 7 && pieces[row+1][col] == null) // if not on botton row and space ahead is unoccupied, add to moves
+                    moves.add( new int[] {row + 1, col} ); 
+             }
+             
+             // check if can capture 
+             if (pieceColor && row != 0){ // if is white
+                 if(col != 7 && pieces[row-1][col+1].pieceColor != pieceColor) // if there's a piece diagonally left to be captured, add to legal moves  
+                     moves.add(new int[] {row -1, col +1}); 
+                 if (col != 0 && pieces[row-1][col-1].pieceColor != pieceColor) // if there's a piece diagonally right to be captured, add to legal moves  
+                     moves.add(new int[] {row -1, col - 1}); 
+             } else if (row != 7){ //piece is black
+                 if(col != 7 && pieces[row+1][col+1].pieceColor != pieceColor) // if there's a piece diagonally left to be captured, add to legal moves  
+                     moves.add(new int[] {row +1, col +1}); 
+                 if (col != 7 && pieces[row+1][col-1].pieceColor != pieceColor) // if there's a piece diagonally right to be captured, add to legal moves  
+                     moves.add(new int[] {row +1, col - 1}); 
+             }
+             
+             //en Passent
+             if ((pieceColor && row == 3) || (!pieceColor && row == 4)){ //if is on row that can en passant for corresponding color 
+                 if(col != 7 && pieces[row][col + 1] instanceof Pawn ){ // check right
+                     Pawn pwn = (Pawn)  pieces[row][col + 1]; 
+                     if(pwn.getCanGetFrenched()) 
+                         moves.add(new int[] {row, col + 1});                      
+                 } 
+                 if (col != 0 && pieces[row][col - 1] instanceof Pawn){ // check left
+                     Pawn pwn = (Pawn)  pieces[row][col - 1]; 
+                     if(pwn.getCanGetFrenched())
+                        moves.add(new int[] {row, col - 1}); 
+                 }
+                 
+             }             
+             
+             // check if put king in check 
+             for(int[] pos : moves){
+                 pieces[pos[0]][pos[1]] = new Pawn(pos[0], pos[1], pieceColor); // piece move to possible position 
+                 pieces[row][col] = null; // remove current piece
+                 if(inCheck()){ // if bad, remove move 
+                     moves.remove(pos); 
+                 }                 
+                 // put piece back 
+                 pieces[pos[0]][pos[1]] = null; 
+                 pieces[row][col] = new Pawn(row, col, pieceColor); 
+             }
+             
+            return moves;
         }
     }
     private class Rook extends Piece {
