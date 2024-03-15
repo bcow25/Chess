ed5b320e3462ad177a7cfa4f22e16a05b15a8663
bcow25 <122178327+bcow25@users.noreply.github.com>
2024-03-15 11:54:30 -0700

    Shop.java

---
+++ Shop.java
@@ -0,0 +1,23 @@
+public class Shop {
+    private int[][] items;
+    public Shop()
+    {
+        
+    }
+    /**
+     * return price of specific piece 
+     */
+    public int getPrice(Object obj)
+    {
+        return -1; //so it compiles
+    }
+    /**
+     * update player's coin count
+     * put item into player's inventory
+     * if unable to buy, print why not
+     */
+    public void buy(Object obj)
+    {
+        
+    }
+}
