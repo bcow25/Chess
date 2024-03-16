163fbf7f89e25fc8b2518d01e2827d465022903e
blgyycw <92336269+blgyycw@users.noreply.github.com>
2024-03-16 00:09:05 -0700

    cleaned up player
    i played myself

--- Player.java
+++ Player.java
@@ -1,62 +1,42 @@
-import java.awt.Graphics;
 import java.awt.Image;
-import java.awt.image.ImageObserver;
 import java.awt.Point;
 import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
 
-public class Player {
+public class Player extends Character {
 
-    // image that represents the player's position on the board
-    private Game game;
-    private Image image;
-    // current position of the player on the board grid
-    private Point pos;
-    // keep track of the player's score
     private int xvel;
     private int yvel;
     public Player(Game game) {
         this.game=game;
         // load the assets
-        loadImage();
+        loadAnimations();
         xvel=0;
         yvel=0;
         // initialize the state
         pos = new Point();
     }
 
-    private void loadImage() {
+    //temp
+    private void loadAnimations() {
         try {
-            // you can use just the filename if the image file is in your
-            // project folder, otherwise you need to provide the file path.
-            image = ImageIO.read(new File("images/player.jpg")).getScaledInstance(50,50,Image.SCALE_DEFAULT);
-            while(image==null); //most sane code written by tracy
+
+            Image killme = ImageIO.read(new File("images/player.jpg")).getScaledInstance(50,50,Image.SCALE_DEFAULT);
+            while(killme==null); //most sane code written by tracy
+            Image[] suicide=new Image[0];
+            suicide[0]=killme;
+            current=new Animator(suicide);
+            walkD=walkL=walkR=walkU=current; //i deserve the death sentence
         } catch (IOException exc) {
             System.out.println("Error opening image file: " + exc.getMessage());
         }
-    }
-
-    public void draw(Graphics g, ImageObserver observer) {
-        // with the Point class, note that pos.getX() returns a double, but 
-        // pos.x reliably returns an int. https://stackoverflow.com/a/30220114/4655368
-        // this is also where we translate board grid position into a canvas pixel
-        // position by multiplying by the tile size.
-        Game b=(Game) observer;
-        g.drawImage(
-            image, 
-            pos.x-b.camera().x+game.getWidth()/2-image.getWidth(game)/2, 
-            pos.y-b.camera().y+game.getHeight()/2-image.getHeight(game)/2, 
-            observer
-        );
     }
     
 
     public void tick() {
         // this gets called once every tick, before the repainting process happens.
         // so we can do anything needed in here to update the state of the player.
-
-        // prevent the player from moving off the edge of the board sideways
         if(game.up) yvel=-10;
         if(game.down) yvel=10;
         if(game.left) xvel=-10;
