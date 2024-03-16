b8d0c299c436930b523bf61d1cbbbe48d460a98b
blgyycw <92336269+blgyycw@users.noreply.github.com>
2024-03-16 00:21:53 -0700

    Update Displayable.java

--- Displayable.java
+++ Displayable.java
@@ -7,7 +7,7 @@ public abstract class Displayable {
     abstract protected Image getImage();
     public void draw(Graphics g,Point p) {
         Image i=getImage();
-        g.drawImage(i,p.x-game.camera().x+game.getWidth()/2-i.getWidth(game)/2,-game.camera().y+game.getHeight()/2-i.getHeight(game)/2,game);
+        g.drawImage(i,p.x-game.camera().x+game.getWidth()/2-i.getWidth(game)/2,p.y-game.camera().y+game.getHeight()/2-i.getHeight(game)/2,game);
     }
     public void draw(Graphics g) {
         draw(g,pos);
