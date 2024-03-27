63225553fdaefe6a9660321cc079428d5ea4fb51
Swing-Rock <85258557+Swing-Rock@users.noreply.github.com>
2024-03-27 12:31:57 -0700

    Update and rename Farm,.java to Farm.java
    make everything static and finished remove plant

---
+++ Farm.java
@@ -0,0 +1,31 @@
+public class Farm {
+    private static Plant[][] farm;
+    public Farm(int r, int c)
+    {
+        farm = new Plant[r][c];
+    }
+    
+    /**
+     * plants plant at row, col
+     */
+    public static void plant (Plant plant, int row, int col)
+    {
+        farm[row][col] = plant;
+    }
+    
+    
+    public static Plant[][] getFarm(){ return farm;} 
+    
+    /**
+     * remove plant at row, col
+     * return plant to inventory
+     */
+    public static void removePlant (int row, int col)
+    {
+        if(farm[row][col] != null){
+            Player.addToInventory(farm[row][col]); 
+            farm[row][col] = null;
+        }
+
+    }
+}
