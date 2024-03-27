dde94eab4d64999b3d0d217e493b3d56844c101c
Swing-Rock <85258557+Swing-Rock@users.noreply.github.com>
2024-03-27 12:51:48 -0700

    Update Shop.java

--- Shop.java
+++ Shop.java
@@ -1,17 +1,31 @@
 public class Shop {
-    private Object[] items;
-
-    public Shop(String type)
+    private Plant[] items;
+    private Player player;
+    public Shop(Game game)
     {
-     if (type == "plant"){items = {}}; //fill with plant options}   
-     else if (type = "pieces") {item = {} }; // still tring to figure out how this work }
+        player = new Player(game);
+        items = new Plant[10];
+        items[0] = new Plant("rose", 2, 2);
+        items[1] = new Plant("sunflower", 3, 2);
+        items[2] = new Plant("marigold", 5, 2);
+        items[3] = new Plant("bush", 10, 2);
+        items[4] = new Plant("orchid", 15, 2);
+        items[5] = new Plant("balloon flower", 20, 1);
+        items[6] = new Plant("peony", 25, 1);
+        items[7] = new Plant("lily", 25, 1);
+        items[8] = new Plant("bird flower", 30, 0);
+        items[9] = new Plant("hydrangea", 30, 0);
     }
     /**
      * return price of specific piece 
+     * return -1 if price not found
      */
-    public int getPrice(Object obj)
+    public int getPrice(Plant pla)
     {
-        return -1; //so it compiles
+        for(Plant p : items){
+            if (p.equals(pla)) return p.getPrice(); 
+        }
+        return -1; 
     }
     /**
      * update player's coin count
@@ -20,6 +34,12 @@ public class Shop {
      */
     public void buy(Object obj)
     {
-        
+        if (player.getNumCoins() < getPrice(obj))
+            System.out.print("You do not have enough coins to buy this item.");
+        else
+        {
+            player.addToInventory((Plant) obj);
+            player.setNumCoins(player.getNumCoins() - getPrice(obj));
+        }
     }
 }
