ed54197af594e6c48a5b04334f88b98e068aefd9
blgyycw <92336269+blgyycw@users.noreply.github.com>
2024-04-02 12:34:16 -0700

    fixed compiler error

--- Tree.java
+++ Tree.java
@@ -1,18 +1,22 @@
 import java.awt.Image;
 import java.awt.Point;
+import javax.imageio.ImageIO;
+import java.io.File;
 public class Tree extends Animatable {
     private Collider collider;
     public Tree(Point pos) {
         loadAnimations();
         this.pos=pos;
-        collider=new Collider(pos,20,70)
+        collider=new Collider(pos,20,70);
     }
     protected void loadAnimations() {
+        try {
         Image[] suicide=new Image[2];
         suicide[0] = ImageIO.read(new File("images/Trees-a.png"));
         suicide[1] = ImageIO.read(new File("images/Trees-b.png"));
         while(suicide[0]==null||suicide[1]==null); //most sane code written by tracy
         current=new Animator(suicide);
+    } catch (Exception e) {System.out.println(e);}
     }
     public void tick() {
         if(Player.get().getCollider().isColliding(collider)) {
