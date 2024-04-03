5ef6a2212e93dad81204bccfea79b0c55fb413a6
Blue-Tides <71620668+Blue-Tides@users.noreply.github.com>
2024-04-03 13:12:07 -0700

    Create NPC.java

---
+++ NPC.java
@@ -0,0 +1,20 @@
+import java.awt.Image;
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.util.ArrayList;
+public class NPC extends Character {
+    private String name;
+    private ArrayList killme;
+    protected void loadAnimations() {
+        try {
+        Image[] suicide=new Image[2];
+        suicide[0] = ImageIO.read(new File("images/np1.png"));
+        suicide[1] = ImageIO.read(new File("images/npc2.png"));
+        while(suicide[0]==null||suicide[1]==null); //most sane code written by tracy
+        walkU=walkD=walkR=walkL=idle=current=new Animator(suicide,200);
+        } catch (Exception e) {System.out.println(e);}
+    }
+    public NPC(String name) {
+        
+    }
+}
