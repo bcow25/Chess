b1ce36d7f6375eff6835bd936d891684d399730b
Blue-Tides <71620668+Blue-Tides@users.noreply.github.com>
2024-04-04 12:55:26 -0700

    Update NPC.java
    im procrastinating

--- NPC.java
+++ NPC.java
@@ -2,9 +2,12 @@ import java.awt.Image;
 import javax.imageio.ImageIO;
 import java.io.File;
 import java.util.ArrayList;
+import java.awt.Point;
 public class NPC extends Character {
     private String name;
     private ArrayList killme;
+    private Collider pusher;
+    private Collider talk;
     protected void loadAnimations() {
         try {
         Image[] suicide=new Image[2];
@@ -14,7 +17,10 @@ public class NPC extends Character {
         walkU=walkD=walkR=walkL=idle=current=new Animator(suicide,200);
         } catch (Exception e) {System.out.println(e);}
     }
-    public NPC(String name) {
-        
+    public NPC(String name, Point pos) {
+        loadAnimations();
+        this.pos=pos;
+        pusher=new Collider(pos,50,50);
+        talk=new Collider(pos,70,70);        
     }
 }
