7a41477e73dcbc790588b8c0abfe8701bebf711e
Swing-Rock <85258557+Swing-Rock@users.noreply.github.com>
2024-04-11 12:46:51 -0700

    Update Board.java
    remove field var moves (test for draws)

--- cheese/Board.java
+++ cheese/Board.java
@@ -1,21 +1,115 @@
 import java.util.ArrayList;
-
 // hi
 public class Board {
     public static Piece[][] pieces;
+    public static boolean inCheck() {
+        King c=whitesTurn?wKing:bKing;
+        for(int i=c.row;i>=0;i--) {
+            if(pieces[i][c.col]==null) continue;
+            if(pieces[i][c.col].pieceColor!=whitesTurn&&pieces[i][c.col] instanceof Rook)
+               return true;
+            if(pieces[i][c.col]!=null) break;
+            
+        }
+        for(int i=c.row;i<8;i++) {
+            if(pieces[i][c.col]==null) continue;
+            if(pieces[i][c.col].pieceColor!=whitesTurn&& pieces[i][c.col] instanceof Rook)
+               return true;
+            if(pieces[i][c.col]!=null) break;
+            
+        }
+        for(int i=c.col;i>=0;i--) {
+            if(pieces[c.row][i]==null) continue;
+            if(pieces[c.row][i].pieceColor!=whitesTurn&& pieces[c.row][i] instanceof Rook)
+               return true;
+            if(pieces[c.row][i]!=null) break;
+            
+        }
+        for(int i=c.col;i<8;i++) {
+            if(pieces[c.row][i]==null) continue;
+            if(pieces[c.row][i].pieceColor!=whitesTurn&& pieces[c.row][i] instanceof Rook)
+               return true;
+            if(pieces[c.row][i]!=null) break;
+            
+        }
+        int j=c.col;
+        for(int i=c.row;i>=0&&j>=0;i--) {
+            if(pieces[i][j]==null) continue;
+            if(pieces[i][j].pieceColor!=whitesTurn&& pieces[i][j] instanceof Bishop)
+               return true;
+            if(pieces[i][j]!=null) break;
+            j--;
+        }
+        
+        j=c.col;
+        for(int i=c.row;i<8&&j>=0;i++) {
+            if(pieces[i][j]==null) continue;
+            if(pieces[i][j].pieceColor!=whitesTurn&&pieces[i][j] instanceof Bishop)
+               return true;
+            if(pieces[i][j]!=null) break;
+            j--;
+        }
+        j=c.col;
+        for(int i=c.row;i>=0&&j<8;i--) {
+            if(pieces[i][j]==null) continue;
+            if(pieces[i][j].pieceColor!=whitesTurn&& pieces[i][j] instanceof Bishop)
+               return true;
+            if(pieces[i][j]!=null) break;
+            j++;
+        }
+        j=c.col;
+        for(int i=c.row;i<8&&j<0;i++) {
+            if(pieces[i][j]==null) continue;
+            if(pieces[i][j].pieceColor!=whitesTurn&& pieces[i][j] instanceof Bishop)
+               return true;
+            if(pieces[i][j]!=null) break;
+            j++;
+        }
+        
+        //Queen stuff
+        int row = c.row; 
+        int col = c.col; 
+        if(row < 7 && row > 0){
+            if (pieces[row + 1][col] instanceof Queen && pieces[row+1][col].pieceColor != whitesTurn && pieces[row - 1][col] == null) return true; 
+            if (pieces[row - 1][col] instanceof Queen && pieces[row-1][col].pieceColor != whitesTurn && pieces[row + 1][col] == null) return true; 
+        }
+        if (col < 7 && col > 0){
+            if (pieces[row][col-1] instanceof Queen && pieces[row][col-1].pieceColor !=whitesTurn && pieces[row][col+1] == null) return true; 
+            if (pieces[row][col+1] instanceof Queen && pieces[row][col+1].pieceColor !=whitesTurn && pieces[row][col-1] == null) return true; 
+        }
+
+        if (col < 7 && col > 0 && row < 7 && row > 0){
+            if (pieces[row - 1][col-1] instanceof Queen && pieces[row-1][col].pieceColor !=whitesTurn && pieces[row+1][col+1] == null) return true; 
+            if (pieces[row - 1][col+1] instanceof Queen && pieces[row-1][col].pieceColor !=whitesTurn && pieces[row+1][col-1] == null) return true; 
+            if (pieces[row + 1][col-1] instanceof Queen && pieces[row+1][col-1].pieceColor !=whitesTurn &&  pieces[row -1 ][col+1] == null) return true; 
+            if (pieces[row + 1][col+1] instanceof Queen && pieces[row+1][col+1].pieceColor !=whitesTurn&&  pieces[row -1][col-1] == null) return true;   
+        }
+        
+        int[][] help={{c.row-2,c.col+1},{c.row-1,c.col+2},{c.row+1,c.col+2},{c.row+2,c.col+1},{c.row-2,c.col-1},{c.row-1,c.col-2},{c.row+1,c.col-2},{c.row+2,c.col-1}};
+        for(int[] i:help)
+            if(i[0]<8&&i[0]>=0&&i[1]<8&&i[1]>=0) if(pieces[i[0]][i[1]] instanceof Knight && pieces[i[0]][i[1]].pieceColor!=whitesTurn) return true;
+        for(int i=c.row-1;i<=c.row+1;i++)
+            for(int k=c.col-1;k<=c.col+1;k++) 
+                if(i>=0&&i<8&&k>=0&&j<8&&pieces[i][k] instanceof King&&pieces[i][k].pieceColor!=c.pieceColor) 
+                    return true;
+        int r=c.row+(c.pieceColor?1:-1);
+        if(r>=0&&r<8&&c.col-1>=0&&c.col-1<8&&pieces[r][c.col-1] instanceof Pawn&&pieces[c.row+(c.pieceColor?1:-1)][c.col-1].pieceColor!=c.pieceColor) 
+            return true;
+        if(r>=0&&r<8&&c.col+1>=0&&c.col+1<8&&pieces[r][c.col+1] instanceof Pawn&&pieces[c.row+(c.pieceColor?1:-1)][c.col+1].pieceColor!=c.pieceColor) 
+            return true;
+        return false;
+    }
     private static boolean whitesTurn;
     public static boolean castleWhite;
     public static boolean castleBlack;
     private static King bKing;
     private static King wKing;
-    private int moves; //for tracking draws
     private ArrayList<Integer> repeatedPositions;
     /**Arrange pieces into the default starting position.**/
     public Board() {
         whitesTurn=true;
         castleWhite=true;
         castleBlack=true;
-        moves=0;
         repeatedPositions=new ArrayList<Integer>();
         bKing=new King(0,4,false);
         wKing=new King(7,4,true);
@@ -28,90 +122,6 @@ public class Board {
             pieces[1][i]=new Pawn(1,i,false);
             pieces[6][i]=new Pawn(6,i,true);
         }
-    }
-    
-    public static boolean inCheck() {
-        King c=whitesTurn?wKing:bKing;
-        
-        int i = c.row;
-        while (i >= 0 && pieces[i][c.col] == null)
-        {
-            if(pieces[i][c.col].pieceColor!=whitesTurn&&(pieces[i][c.col] instanceof Queen || pieces[i][c.col] instanceof Rook))
-               return true;
-            i--;
-        }
-        i = c.row;
-        while (i < 8 && pieces[i][c.col] == null)
-        {
-            if(pieces[i][c.col].pieceColor!=whitesTurn&&(pieces[i][c.col] instanceof Queen || pieces[i][c.col] instanceof Rook))
-               return true;
-            i++;
-        }
-        i = c.col;
-        while (i >= 0 && pieces[i][c.col] == null)
-        {
-            if(pieces[c.row][i].pieceColor!=whitesTurn&&(pieces[c.row][i] instanceof Queen || pieces[c.row][i] instanceof Rook))
-               return true;
-            i--;
-        }
-        i = c.col;
-        while (i < 8 && pieces[i][c.col] == null)
-        {
-            if(pieces[c.row][i].pieceColor!=whitesTurn&&(pieces[c.row][i] instanceof Queen || pieces[c.row][i] instanceof Rook))
-               return true;
-            i++;
-        }
-        int j = c.col;
-        i = c.row;
-        while (i >= 0 && j >= 0 && pieces[i][c.col] == null)
-        {
-            if(pieces[i][j].pieceColor!=whitesTurn&&(pieces[i][j] instanceof Queen || pieces[i][j] instanceof Bishop))
-               return true;
-            i--;
-            j--;
-        }
-        j = c.col;
-        i = c.row;
-        while (i < 8 && j >= 0 && pieces[i][c.col] == null)
-        {
-            if(pieces[i][j].pieceColor!=whitesTurn&&(pieces[i][j] instanceof Queen || pieces[i][j] instanceof Bishop))
-               return true;
-            i++;
-            j--;
-        }
-        j = c.col;
-        i = c.row;
-        while (i >= 0 && j < 8 && pieces[i][c.col] == null)
-        {
-            if(pieces[i][j].pieceColor!=whitesTurn&&(pieces[i][j] instanceof Queen || pieces[i][j] instanceof Bishop))
-               return true;
-            i--;
-            j++;
-        }
-        j = c.col;
-        i = c.row;
-        while (i < 8 && j < 0 && pieces[i][c.col] == null)
-        {
-            if(pieces[i][j].pieceColor!=whitesTurn&&(pieces[i][j] instanceof Queen || pieces[i][j] instanceof Bishop))
-               return true;
-            i++;
-            j++;
-        }
-        int[][] help={{c.row-2,c.col+1},{c.row-1,c.col+2},{c.row+1,c.col+2},{c.row+2,c.col+1},{c.row-2,c.col-1},{c.row-1,c.col-2},{c.row+1,c.col-2},{c.row+2,c.col-1}};
-        for(int[] x : help)
-            if(x[0] < 8 && x[0] >= 0 && x[1] < 8 && x[1] >= 0) 
-                if(pieces[x[0]][x[1]] instanceof Knight && pieces[x[0]][x[1]].pieceColor != whitesTurn) 
-                    return true;
-        for(int r = c.row - 1; r <= c.row + 1; i++)
-            for(int k = c.col - 1; k <= c.col + 1; k++) 
-                if(r >= 0 && r < 8 && k >= 0 && k <8 && pieces[i][k] instanceof King && pieces[i][k].pieceColor != c.pieceColor) 
-                    return true;
-        int r=c.row+(c.pieceColor?1:-1);
-        if(r >= 0 && r < 8 && c.col - 1 >= 0 && c.col - 1 < 8 && pieces[r][c.col - 1] instanceof Pawn && pieces[c.row + (c.pieceColor?1:-1)][c.col - 1].pieceColor != c.pieceColor) 
-            return true;
-        if(r >= 0 && r < 8 && c.col + 1 >= 0 && c.col + 1 < 8 && pieces[r][c.col + 1] instanceof Pawn && pieces[c.row + (c.pieceColor?1:-1)][c.col + 1].pieceColor != c.pieceColor) 
-            return true;
-        return false;
     }
     public static Piece[][] getBoard() {return pieces;}
     public static boolean contains(Piece p){
