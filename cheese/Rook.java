c4d18c9ffbd44dbc61c6466d0e282cf7e62d2b48
Swing-Rock <85258557+Swing-Rock@users.noreply.github.com>
2024-04-12 12:04:52 -0700

    Update Rook.java
    rook doesn't jump over pieces now

--- cheese/Rook.java
+++ cheese/Rook.java
@@ -11,43 +11,66 @@ import java.util.ArrayList;
         }
         
         //moves to pieces[r][c]
-        public void move(int r, int c) {
-            super.move(r, c);
-            if (pieceColor)
-                Board.castleWhite = false;
-            else 
-                Board.castleBlack = false;
+        public void move(int r, int c, boolean test) {
+            if(test){
+                super.move(r, c, false);
+            } else{
+                super.move(r, c, false);
+                if (pieceColor)
+                    Board.castleWhite = false;
+                else 
+                    Board.castleBlack = false;
+            }
         }
         public String toString (){ return "rook: " + pieceColor;}
        public ArrayList<int[]> generateLegalMoves() {
             ArrayList<int[]> ans = new ArrayList<int[]>();
             int r = row;
-            while (r >= 1 ) { //checking squares left of current position
+            while (r >= 1 ) { //checking squares above of current position
                 r--;
                 if(Board.pieces[r][col] != null && Board.pieces[r][col].pieceColor == pieceColor)break; 
-                if ((Board.pieces[r][col] == null || (Board.pieces[r][col] != null && Board.pieces[r][col].pieceColor != pieceColor )) && testMove(r, col))
+                if(Board.pieces[r][col] != null && Board.pieces[r][col].pieceColor != pieceColor  &&  testMove(r, col)){
+                    ans.add(new int[] {r, col} ); 
+                    break; 
+                }
+                if (Board.pieces[r][col] == null && testMove(r, col))
                     ans.add(new int[] {r, col} );               
             }
             r = row;
-            while (r <= 6 ) { //checking squares right of current position
+            while (r <= 6 ) { //checking squares below of current position
                 r++; 
                 if(Board.pieces[r][col] != null && Board.pieces[r][col].pieceColor == pieceColor)break; 
+                if(Board.pieces[r][col] != null && Board.pieces[r][col].pieceColor != pieceColor  &&  testMove(r, col)){
+                    ans.add(new int[] {r, col} ); 
+                    break; 
+                }
                 if ((Board.pieces[r][col] == null || (Board.pieces[r][col] != null && Board.pieces[r][col].pieceColor != pieceColor ) ) && testMove(r,col))
                     ans.add(new int[] {r, col} );
             }
             int c = col;
-            while (c >= 1) { //checking squares above current position
+            while (c >= 1) { //checking squares left current position
                 c--; 
                 if(Board.pieces[row][c] != null && Board.pieces[row][c].pieceColor == pieceColor)break; 
-                if ((Board.pieces[row][c] == null || (Board.pieces[row][c] != null && Board.pieces[row][c].pieceColor != pieceColor )) && testMove(row,col)) 
+                if(Board.pieces[row][c] != null && Board.pieces[row][c].pieceColor != pieceColor  &&  testMove(row, c)){
+                    ans.add(new int[] {row, c} ); 
+                    break; 
+                }
+                if ((Board.pieces[row][c] == null || (Board.pieces[row][c] != null && Board.pieces[row][c].pieceColor != pieceColor )) && testMove(row,c)) 
                     ans.add(new int[] {row, c} );
             }
             c = col;
-            while (c <= 6 ) { //checking squares below current position
+            while (c <= 6 ) { //checking squares right current position
                 c++; 
                 if(Board.pieces[row][c] != null && Board.pieces[row][c].pieceColor == pieceColor)break; 
-                if ((Board.pieces[row][c] == null || (Board.pieces[row][c] != null && Board.pieces[row][c].pieceColor != pieceColor )) && testMove(row,col)) 
-                    ans.add(new int[] {row, c} );
+                if(Board.pieces[row][c] != null && Board.pieces[row][c].pieceColor != pieceColor  &&  testMove(row, c)){
+                    ans.add(new int[] {row, c} ); 
+                    break; 
+                }
+                if ((Board.pieces[row][c] == null || (Board.pieces[row][c] != null && Board.pieces[row][c].pieceColor != pieceColor ))){
+                    if(testMove(row,c)) 
+                        ans.add(new int[] {row, c} );
+                }
+                
             }
             return ans;
         }
