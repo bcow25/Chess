94c4d6fbd610d391db34cb01b621b76d8fa54956
Swing-Rock <85258557+Swing-Rock@users.noreply.github.com>
2024-05-03 12:50:59 -0700

    Update ChessGame.java

--- cheese/ChessGame.java
+++ cheese/ChessGame.java
@@ -32,6 +32,7 @@ public class ChessGame{
         } 
         if (Board.endGame() == 1){ // if white (player) wins
             Player.changeNumCoins(3); 
+            System.out.println("You won! You earned 3 coins");
         } else if (Board.endGame() == 2 || giveUp){ // if player lost or give up 
             System.out.println("Tough Luck"); 
         }
@@ -63,10 +64,16 @@ public class ChessGame{
     public Object[] selectPiece(){
         ///CLI interface, 4 testing
         Scanner in = new Scanner(System.in);
-        System.out.print("See move of piece (row):");
-        int r = in.nextInt();
-        System.out.print("See move of piece (col):");
-        int c = in.nextInt();
+        int r; 
+        int c; 
+        do{
+            System.out.print("See move of piece (row):");
+            r = in.nextInt();
+            System.out.print("See move of piece (col):");
+            c = in.nextInt();
+        } while (Board.getBoard()[r][c] == null);
+        
+        
         
         Object[] ans = new Object [3]; 
         Piece chosen = Board.getBoard()[r][c]; //player click on the piece
@@ -93,9 +100,8 @@ public class ChessGame{
                     ans[0] = chosen; //set ans[0] to the piece chosen
                     ans[1] = playerSelection[0]; // set ans[1] to the row
                     ans[2] = playerSelection[1]; //set ans[2] to the col
-                    System.out.println("d");
                     break; 
-                } else if (Board.getBoard()[playerSelection[0]][playerSelection[1]]!= null && Board.getBoard()[playerSelection[0]][playerSelection[1]].pieceColor){ 
+                } else if (Board.getBoard()[playerSelection[0]][playerSelection[1]]!= null && onBoard(playerSelection) && Board.getBoard()[playerSelection[0]][playerSelection[1]].pieceColor){ 
                     // else if player selected another piece they own on the board
                     chosen = Board.getBoard()[playerSelection[0]][playerSelection[1]]; // set chosen to the piece they selected and loop again 
                     break; 
@@ -109,12 +115,17 @@ public class ChessGame{
                     System.out.print("you selected" + playerSelection[0] + ", " + playerSelection[1]);
                 }
             }
-            System.out.println("c"); 
                 
         } while (ans[0] == null) ; 
-        System.out.println("b");
         return ans;          
     }   
+    
+    //precondition: move is a int array with len of 2
+    //move[0] is row and move[1] is col
+    //return if move is a possible square on board 
+    public boolean onBoard(int[] move){
+        return move[0] <= 7 && move[0]>= 0 && move[1] >=0 && move[1] <=7; 
+    }
     
     //input: array list of int array and int array
     //returns if container contains target 
