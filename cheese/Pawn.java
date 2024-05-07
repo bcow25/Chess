fb9da11b6e8932945fb6f314c19ced6588b63f29
Swing-Rock <85258557+Swing-Rock@users.noreply.github.com>
2024-05-07 12:35:33 -0700

    Update Pawn.java
    clean up

--- cheese/Pawn.java
+++ cheese/Pawn.java
@@ -22,20 +22,16 @@ public class Pawn extends Piece {
             if(test){
                 super.move(r, c, false); 
             } else{ 
-                if(r == row + 2 || r == row - 2){
-                    canGetFrenched = true; 
-                }else{
-                    canGetFrenched = false; 
-                }
-                Board.pieces[r][c]=this;
-                Board.pieces[row][col]=null;
+                canGetFrenched = r == row + 2 || r == row - 2;
+                Board.getBoard()[r][c]=this;
+                Board.getBoard()[row][col]=null;
                 firstMove = false; 
                 row=r;
                 col=c;
                 if(pieceColor && row == 0){ // if is white and reach end 
-                    Board.pieces[row][col]= new Queen(row, col, pieceColor);                     
+                    Board.getBoard()[row][col]= new Queen(row, col, pieceColor);                     
                 } else if(!pieceColor && row == 7){ // if is white and reach end 
-                    Board.pieces[row][col]= new Queen(row, col, pieceColor);                     
+                    Board.getBoard()[row][col]= new Queen(row, col, pieceColor);                     
                 }
             }
         }
@@ -45,13 +41,13 @@ public class Pawn extends Piece {
             ArrayList<int[]> moves = new ArrayList<int[]>(); 
             if(firstMove){//if is Pawn's first move then can move two blocks 
                 if(pieceColor){ // if is white 
-                    if(Board.pieces[row-2][col] == null) // if 2 space ahead is unoccupied, add to moves
+                    if(Board.getBoard()[row-2][col] == null) // if 2 space ahead is unoccupied, add to moves
                         if(testMove(row-2,col))
                         {   moves.add( new int[] {row - 2, col} );      
                         
                         }               
                 } else { //color is black 
-                    if(Board.pieces[row+2][col] == null) // if 2 space ahead is unoccupied, add to moves
+                    if(Board.getBoard()[row+2][col] == null) // if 2 space ahead is unoccupied, add to moves
                         if(testMove(row+2,col)){
                             moves.add( new int[] {row + 2, col} );
                         } 
@@ -59,12 +55,12 @@ public class Pawn extends Piece {
             }
              //check if pawn can move forward
              if(pieceColor){// if is white
-                 if(row != 0 && Board.pieces[row-1][col] == null) // if not on top row and space ahead is unoccupied, add to moves
+                 if(row != 0 && Board.getBoard()[row-1][col] == null) // if not on top row and space ahead is unoccupied, add to moves
                     if(testMove(row-1,col)){
                         moves.add( new int[] {row - 1, col} );
                     } 
              } else { //color is black 
-                 if(row != 7 && Board.pieces[row+1][col] == null) // if not on botton row and space ahead is unoccupied, add to moves
+                 if(row != 7 && Board.getBoard()[row+1][col] == null) // if not on botton row and space ahead is unoccupied, add to moves
                     if(testMove(row+1,col)){
                         moves.add( new int[] {row + 1, col} );
                     } 
@@ -72,26 +68,26 @@ public class Pawn extends Piece {
              
              // check if can capture 
              if (pieceColor && row != 0){ // if is white
-                 if(col != 7 &&  Board.pieces[row-1][col+1] != null && Board.pieces[row-1][col+1].pieceColor != pieceColor) // if there's a piece diagonally left to be captured, add to legal moves  
+                 if(col != 7 &&  Board.getBoard()[row-1][col+1] != null && Board.getBoard()[row-1][col+1].pieceColor != pieceColor) // if there's a piece diagonally left to be captured, add to legal moves  
                      if(testMove(row-1,col+1)) moves.add(new int[] {row -1, col +1}); 
-                 if (col != 0 && Board.pieces[row-1][col-1] != null && Board.pieces[row-1][col-1].pieceColor != pieceColor) // if there's a piece diagonally right to be captured, add to legal moves  
+                 if (col != 0 && Board.getBoard()[row-1][col-1] != null && Board.getBoard()[row-1][col-1].pieceColor != pieceColor) // if there's a piece diagonally right to be captured, add to legal moves  
                      if(testMove(row-1,col-1)) moves.add(new int[] {row -1, col - 1}); 
              } else if (row != 7){ //piece is black
-                 if(col != 7 && Board.pieces[row+1][col+1] != null && Board.pieces[row+1][col+1].pieceColor != pieceColor) // if there's a piece diagonally left to be captured, add to legal moves  
+                 if(col != 7 && Board.getBoard()[row+1][col+1] != null && Board.getBoard()[row+1][col+1].pieceColor != pieceColor) // if there's a piece diagonally left to be captured, add to legal moves  
                      if(testMove(row+1,col+1)) moves.add(new int[] {row +1, col +1}); 
-                 if (col != 0 && Board.pieces[row+1][col-1] != null && Board.pieces[row+1][col-1].pieceColor != pieceColor) // if there's a piece diagonally right to be captured, add to legal moves  
+                 if (col != 0 && Board.getBoard()[row+1][col-1] != null && Board.getBoard()[row+1][col-1].pieceColor != pieceColor) // if there's a piece diagonally right to be captured, add to legal moves  
                      if(testMove(row+1,col-1)) moves.add(new int[] {row +1, col - 1}); 
              }
              
              //en Passent
              if ((pieceColor && row == 3) || (!pieceColor && row == 4)){ //if is on row that can en passant for corresponding color 
-                 if(col != 7 && Board.pieces[row][col + 1] instanceof Pawn ){ // check right
-                     Pawn pwn = (Pawn)  Board.pieces[row][col + 1]; 
+                 if(col != 7 && Board.getBoard()[row][col + 1] instanceof Pawn ){ // check right
+                     Pawn pwn = (Pawn)  Board.getBoard()[row][col + 1]; 
                      if(pwn.getCanGetFrenched()) 
                          if(testMove(row,col+1)) moves.add(new int[] {row, col + 1});                      
                  } 
-                 if (col != 0 && Board.pieces[row][col - 1] instanceof Pawn){ // check left
-                     Pawn pwn = (Pawn)  Board.pieces[row][col - 1]; 
+                 if (col != 0 && Board.getBoard()[row][col - 1] instanceof Pawn){ // check left
+                     Pawn pwn = (Pawn)  Board.getBoard()[row][col - 1]; 
                      if(pwn.getCanGetFrenched())
                         if(testMove(row,col-1)) {
                             moves.add(new int[] {row, col - 1});
