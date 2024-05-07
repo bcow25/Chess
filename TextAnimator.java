44ed17df2a8afe8f28f5b49c7d712e25a932bc2d
blgyycw <92336269+blgyycw@users.noreply.github.com>
2024-05-07 12:51:53 -0700

    Update TextAnimator.java

--- TextAnimator.java
+++ TextAnimator.java
@@ -6,11 +6,20 @@ public class TextAnimator {
     public TextAnimator(String text, int speed) {
         this.text=text;
         this.speed=speed;
-        animatedText="";
+        reset();
         time=0;
     }
     public TextAnimator(String text) {
-        this(text,50);
+        this(text,1000);
+    }
+    public String getFrame() {
+        if(animatedText.length()==text.length()) return text;
+        long t=System.currentTimeMillis();
+        for(int i=0;i<(t-time)/speed;i++) {
+            animatedText+=text.charAt(animatedText.length());
+        }
+        time=t;
+        return animatedText;
     }
     public void reset() {
         animatedText="";
