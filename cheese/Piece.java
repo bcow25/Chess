daa4844c16fc7f0702f55a2aea811bdc5e4bdcbe
Swing-Rock <85258557+Swing-Rock@users.noreply.github.com>
2024-05-16 12:50:18 -0700

    Update Piece.java

--- cheese/Piece.java
+++ cheese/Piece.java
@@ -1,19 +1,47 @@
 import java.util.*;
+import java.awt.Image;
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.io.IOException;
 
-public abstract class Piece {
+
+public abstract class Piece extends Displayable{
+        protected abstract void loadImage(); 
+    
         public Piece (int r, int c,boolean color){
             row = r; 
             col = c;
             pieceColor=color;
+            loadImage(); 
         }
         protected boolean pieceColor; //true means white
         protected int row;
         protected int col;
+        
         public void move(int r,int c, boolean test) {
             Board.getBoard()[r][c]=this;
             Board.getBoard()[row][col]=null;
+            if(row < r){
+                for(int i = row; i < r; i++){
+                    pos.y += Board.getSquareSize(); 
+                }
+            } else{
+                for(int i = r; i < row; i++){
+                    pos.y -= Board.getSquareSize(); 
+                }
+            }
+            if(col < c){
+                for(int i = col; i < c; i++){
+                    pos.x += Board.getSquareSize(); 
+                }
+            } else{
+                for(int i = c; i < col; i++){
+                    pos.x -= Board.getSquareSize(); 
+                }
+            }
             row=r;
             col=c;
+            
         }
          //test if a move will put your king in check,out of bounds, or collides with your own piece
         //true means is a valid move
