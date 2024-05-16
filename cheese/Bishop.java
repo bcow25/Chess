793feb7fd08975a28ca14863806d996bc8dc4df1
Swing-Rock <85258557+Swing-Rock@users.noreply.github.com>
2024-05-16 12:47:46 -0700

    Update Bishop.java

--- cheese/Bishop.java
+++ cheese/Bishop.java
@@ -1,4 +1,9 @@
 import java.util.ArrayList;
+import java.awt.Image;
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.io.IOException;
+import java.awt.Point;
 
 /**
      * BISHOP:
@@ -6,11 +11,31 @@ import java.util.ArrayList;
      */
    
     public class Bishop extends Piece {
+        private Image killme;
+        protected Image getImage(){
+            return killme; 
+        }
+        protected void loadImage() {
+            try {
+    
+                killme = ImageIO.read(new File("images/player.jpg")).getScaledInstance(50,50,Image.SCALE_DEFAULT);
+                while (killme == null); //most sane code written by tracy
+                } catch (IOException exc) {
+                System.out.println("Error opening image file: " + exc.getMessage());
+            }
+        }
+        
         public Bishop(int r, int c,boolean color){
-            super(r,c,color); 
+            super(r,c,color);
+            pos=new Point(0,0); 
+             
         }
         @Override
-        public String toString (){return "bishop:" + pieceColor;}
+        public String toString (){  
+           if(pieceColor){
+                return("White Bishop at " + row + ", " + col); 
+            } else {return("Black Bishop at " + row + ", " + col); }
+        }
         @Override
         public ArrayList<int[]> generateLegalMoves() {
             ArrayList<int[]> ans = new ArrayList<int[]>();
