43f005154e9ce36ab155042807fbfd5624eda440
Blue-Tides <71620668+Blue-Tides@users.noreply.github.com>
2024-05-21 15:23:04 -0700

    made hitbox not drawable

--- Hitbox.java
+++ Hitbox.java
@@ -1,24 +1,18 @@
 import java.awt.*;
 import javax.imageio.ImageIO;
 import java.io.*;
-public class Hitbox extends Displayable implements Tickable {
-    private Image img;
+public class Hitbox implements Tickable {
+    private Collider collider;
     public Hitbox(String str,Point pos) {
-        super(pos,0,0,0,0);
         try {
-                img = ImageIO.read(new File(str));
-                
+                Image img = ImageIO.read(new File(str));
+                collider=new Collider(pos,img.getWidth(Game.get()),img.getHeight(Game.get()));
             } catch (IOException e) {
                 // TODO Auto-generated catch block
                 e.printStackTrace();
             }
-        collider.setH(img.getHeight(Game.get()));
-        collider.setW(img.getWidth(Game.get()));
-        display.setH(collider.getH());
-        display.setW(collider.getW());
     }
-    
-    public Image getImage() {return img;}
+       
     public void tick() {
         collider.pushPlayer();
     }
