24598847c4623608f2c30da89c40685b862cbd1c
Swing-Rock <85258557+Swing-Rock@users.noreply.github.com>
2024-05-21 12:52:17 -0700

    Update Plant.java

--- Plant.java
+++ Plant.java
@@ -1,18 +1,16 @@
 public class Plant {
-    private int price;
+    
     private String name; 
     @SuppressWarnings("unused")
     private int rarity; //0 is epic, 1 is rare, 2 is common
+    private int ind; 
     
-    
-    public Plant(String type, int p, int r)
+    public Plant(String type, int r)
     {
-        price = p; 
         name = type; 
         rarity = r; 
     }
     
-    public int getPrice(){return price;}
     public String getName(){return name; } 
     
     public boolean equals(Plant p){
