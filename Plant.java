64bb70662bb8852ca622dbd8615be3fe7e9bc577
Blue-Tides <71620668+Blue-Tides@users.noreply.github.com>
2024-05-21 15:16:06 -0700

    plant

--- Plant.java
+++ Plant.java
@@ -1,14 +1,23 @@
+import java.awt.Image;
+
 public class Plant {
     
     private String name; 
     @SuppressWarnings("unused")
     private int rarity; //0 is epic, 1 is rare, 2 is common
-    private int ind; 
+    private boolean seed; //true -> is a seed, false -> grown
+    private static String[] types={"Balloon","Bird","Bush","Hydrangea","Marigold","Orchid","Peony","Rose","Sunflower"};
+    private static int[] raritys={1,0,2,0,2,2,1,2,2};
+    private static Image[] images=new Image[raritys.length];
     
-    public Plant(String type, int r)
-    {
-        name = type; 
-        rarity = r; 
+    
+    public Plant(int i) {
+        name = types[i]; 
+        rarity=raritys[i];
+        seed=false;
+    }
+    public Plant() {
+        this((int)(Math.random()*types.length));
     }
     
     public String getName(){return name; } 
