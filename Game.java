0849cdf8cc6df394ad972ddb86ba34e85ae9be0d
Blue-Tides <71620668+Blue-Tides@users.noreply.github.com>
2024-05-23 21:22:04 -0700

    delete bad files

--- Game.java
+++ Game.java
@@ -353,7 +353,7 @@ public class Game extends JPanel implements ActionListener, KeyListener {
             o1=false;
         if (key==KeyEvent.VK_2)
             o2=false;
-        if(key==KeyEvent.VK_3) TestChess.main(new String[]{"1"});
+       // if(key==KeyEvent.VK_3) TestChess.main(new String[]{"1"});
         if(key==KeyEvent.VK_4) System.out.println(Player.get().getPos());
     }
 
